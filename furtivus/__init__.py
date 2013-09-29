80354c9269e6c8a948eea951d21db31a8f311511
joe di castro <joe@joedicastro.com>
2013-09-29 20:22:53 +0200

    Add the project basic structure

---
+++ furtivus/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+#   __            _   _
+#  / _|_   _ _ __| |_(_)_   ___   _ ___
+# | |_| | | | '__| __| \ \ / / | | / __|
+# |  _| |_| | |  | |_| |\ V /| |_| \__ \
+# |_|  \__,_|_|   \__|_| \_/  \__,_|___/
+
+
+
+"""
+furtivus
+~~~~~~~~
+
+A Linux hidden files (AKA dotfiles) manager
+
+"""
+
+
+__title__ = "furtivus"
+__version__ = "0.0.1"
+__author__ = "joe di castro <joe@joedicastro.com>"
+__license__ = "MIT"
+__copyright__ = "Copyright 2013 joe di castro"
